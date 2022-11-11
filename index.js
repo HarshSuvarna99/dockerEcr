90ab6f25fd69d3a9e3cd6358fc2bf135225cfbed
Ashish_Gupta <ashish.gupta@codeb.online>
2022-11-11 15:06:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const port=5000;
+
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    // console.log("hello world")
+    res.send('hello world')
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running at: http://localhost:${port}`);
+})
